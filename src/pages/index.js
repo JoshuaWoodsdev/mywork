7a863d32e58cbb551082547db3aa54d5e830800d
JoshuaWoodsdev <nordev@protonmail.com>
2024-02-27 06:03:12 +0000

    push

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,17 +9,17 @@ const IndexPage = () => {
       <article>
         <header class="bg-washed-yellow sans-serif">
           <div class="mw9 center pa4 pt5-ns ph7-l">
-            <h3 class="f2 f1-m f-headline-l measure-narrow lh-title mv0">
+            <h3 class="f1 f1-m f-headline-l measure-narrow lh-title mv0">
               <span class="bg-black-90 lh-copy white pa1 tracked-tight">
                 {data.home.title}
               </span>
             </h3>
-            <h4 class="f3 fw1 georgia i">{data.home.cta}</h4>
+            <h4 class="f2 fw1 georgia i">{data.home.cta}</h4>
           </div>
         </header>
 
         <div class="pa4 ph7-l georgia mw9-l center">
-          <p class="f5 f4-ns lh-copy measure">
+          <p class="f3 f2-ns  measure">
             {data.home.description}
           </p>
         </div>
